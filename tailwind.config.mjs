1f819f18f09ec8825eee010ab4d1d4e13bc43078
CEhM <ceduardo.eh@gmail.com>
2024-12-26 23:35:19 -0500

    add Tailwind CSS integration and global styles; update layout and components

---
+++ tailwind.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class',
+	theme: {
+		extend: {
+			colors: {
+				background: 'rgba(var(--background))',
+				main: 'rgba(var(--main))',
+				secondary: 'rgba(var(--secondary))',
+				accent: 'rgba(var(--accent))',
+				'reverse-accent': 'rgba(var(--reverse-accent))',
+			},
+		},
+	},
+	plugins: [],
+}
